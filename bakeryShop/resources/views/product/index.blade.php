7f35091236099ce05237ccc2c52ab069be1a709b
justatea <s160719008@student.ubaya.ac.id>
2022-01-01 16:19:09 +0700

    up

--- bakeryShop/resources/views/product/index.blade.php
+++ bakeryShop/resources/views/product/index.blade.php
@@ -5,10 +5,29 @@
     <h2>Product</h2>
     <div class="row">
         @foreach($data as $d)
+        <a class="btn btn-default"  href="#detail_{{$d->id}}" 
+          data-toggle="modal">{{ $d->nama_produk }}</a>  
+        </td>
+        <div class="modal fade" id="detail_{{$d->id}}" tabindex="-1" role="basic" aria-hidden="true">
+          <div class="modal-dialog">
+             <div class="modal-content">
+                <div class="modal-header">
+                   <h4 class="modal-title">{{$d->nama_produk}}</h4>
+                </div>
+                <div class="modal-body">
+                   <img src='images/{{ $d->id }}.jpg' height='200px' />
+                </div>
+          <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+             </div>
+          </div>
+        </div>
+        
         <div class="col-md-4 col-sm-6">
             <div class="product-grid">
                 <div class="product-image"> 
-                    <a href="/product/{{$d->id}}"> <img class="pic-1" src="{{ asset('images'.$d->id)}}"></a>
+                    <a href="/product/{{$d->id}}"> <img class="pic-1" src="{{url('assets/img/product/'.$d->id.'.jpg')}}" style="width:200px"></a>
                 </div>
                 <div class="product-content">
                     <h3 class="title"><a href="/product/{{$d->id}}">{{$d->nama_produk}}</a></h3> <span class="price">{{$d->harga_produk}}</span>
@@ -18,4 +37,6 @@
         @endforeach
     </div>
 </div>
+
+
 @endsection
--- bakeryShop/resources/views/welcome.blade.php
+++ bakeryShop/resources/views/welcome.blade.php
@@ -31,6 +31,26 @@
             <a href="/category">Category</a>
             <a href="/product">Product</a>
             <a href="https://laravel-news.com">News</a>
+            <a class="btn btn-default" data-toggle="modal" href="#disclaimer">Disclaimer</a>
+
         </div>
     </div>
+
+    <div class="modal fade" id="disclaimer" tabindex="-1" role="basic" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title">DISCLAIMER</h4>
+              </div>
+              <div class="modal-body">
+                Pictures shown are for illustration purpose only.Actual product may vary due to product enhancement. 
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+         </div>
+      </div>
+      
 @endsection
